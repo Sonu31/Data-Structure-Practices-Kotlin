6eaffabf00b64b905e760a734103117481e9a9f7
Sonu Kumar <sonuhjpkumar@gmail.com>
2025-04-25 01:02:27 +0530

    Done

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin_Android_DSA_Experiment"
+include(":app")
+include(":jetpack:Test")
+include(":KotlinDSA:Test")
+//include(":DSA:Test")
+include(":Android:Experiment")
